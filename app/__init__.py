089e973b799113e6f7db7996e8a0a25f91cc2794
Nicolas <nicolasimbe2006@gmail.com>
2025-10-20 15:02:55 -0300

    cria fabrica de aplicacoes Flask com SQLAlchemy

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_class=Config):
+    app = Flask(__name__, instance_relative_config=True)
+    
+    app.config.from_object(config_class)
+    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    from app import routes
+    app.register_blueprint(routes.bp)
+    
+    return app
+ 
