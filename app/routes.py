ba410ee984ad39520981200fdd69d1c2279a9d7f
Nicolas <nicolasimbe2006@gmail.com>
2025-10-21 14:49:50 -0300

    adiciona templates html, utilizando jinja para reutilizacao de codigo com blocos, configura routes para mostrar na tela um dashboard simples

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from app.models import Empresa
 #esqueleto do routes para rodar a criação do banco, rotas serão definidas aqui posteriormente
 
 bp = Blueprint('main', __name__)
+
+@bp.route('/')
+def dashboard():
+    empresas = Empresa.query.order_by(Empresa.nome).all()
+    
+    return render_template('dashboard.html', empresas=empresas)
